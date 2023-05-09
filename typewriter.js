c72848439e3aadc2912b8e04114c6985a01cc761
Lindsay Ward <lward23@hotmail.com>
2023-05-09 12:12:35 -0700

    Add console.log after writing last character of sentence

--- typewriter.js
+++ typewriter.js
@@ -2,7 +2,12 @@ const sentence = "hello there from Lighthouse Labs";
 let delay = 0;
 for (const char in sentence) {
   setTimeout(() => {
+    if (char == (sentence.length - 1)) {
+      process.stdout.write(sentence[char]);
+      console.log();
+    } else {
     process.stdout.write(sentence[char]);
+    }
   }, delay);
   delay += 50;
 }
